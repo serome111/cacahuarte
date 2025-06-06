06bde241f5e0a614a35f907db733572eaff69b4f
Serome111 <serome111@gmail.com>
2025-06-06 15:21:36 -0500

    updated for laravel 12

--- resources/views/partials/faq.blade.php
+++ resources/views/partials/faq.blade.php
@@ -4,22 +4,29 @@
 
     <div class="section-title">
       <h2 data-aos="fade-up">F.A.Q</h2>
-      <p data-aos="fade-up">Aqui podras encontrar las preguntas mas frecuentes de nuestros.</p>
+      <p data-aos="fade-up">Aqui podras encontrar las preguntas mas frecuentes de nuestros productos.</p>
     </div>
     <div class="faq-list">
-      <ul>
-        @for($i=0; $i < count($faqs); $i++)
-            <li data-aos="fade-up" data-aos-delay="{{$i}}00">
-              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" href="#" class="collapse" data-bs-target="#faq-list-{{$i}}">{{ $faqs[$i]->question}} <i class="bx bx-chevron-down icon-show"></i></a>
-              <div id="faq-list-{{$i}}" class="collapse" data-bs-parent=".faq-list">
-                <p>
-                  {{ $faqs[$i]->solution }}
-                  <a href="{{ $faqs[$i]->link }}" class="details-link" title="Mas detalles"><i class="bx bx-link">{{ $faqs[$i]->textLink }}</i></a>
-                </p>
-              </div>
-            </li>
-        @endfor
-      </ul>
+    <ul>
+    @for($i = 0; $i < count($faqs); $i++)
+        <li data-aos="fade-up" data-aos-delay="{{ $i }}00">
+            <i class="bx bx-help-circle icon-help"></i>
+            <a data-bs-toggle="collapse" href="#" class="collapse" data-bs-target="#faq-list-{{ $i }}">
+                {{ $faqs[$i]->question }}
+                <i class="bx bx-chevron-down icon-show"></i>
+            </a>
+            <div id="faq-list-{{ $i }}" class="collapse" data-bs-parent=".faq-list">
+                <p>{{ $faqs[$i]->solution }}</p>
+                @if($faqs[$i]->link)
+                    <a href="{{ $faqs[$i]->link }}" class="details-link" title="Más detalles" target="_blank" rel="noopener">
+                        <i class="bx bx-link"></i> {{ $faqs[$i]->textLink }}
+                    </a>
+                @endif
+            </div>
+        </li>
+    @endfor
+</ul>
+
     </div>
   </div>
 </section><!-- End F.A.Q Section -->
